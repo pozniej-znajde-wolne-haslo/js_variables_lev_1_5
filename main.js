8c77cf3a23fbe160e085d4407b77de4b348c184c
pozniej-znajde-wolne-haslo <aniarafeld@o2.pl>
2023-01-03 14:55:10 +0100

    js_variables_1_5

---
+++ main.js
@@ -0,0 +1,20 @@
+let carName = "BMW";
+console.log(carName);
+
+let x = 150;
+console.log(x);
+
+let y = 50;
+console.log(y);
+
+let z = x+y;
+console.log(z);
+
+// mehrere Variablen in einer Zeile deklarieren (NAMEN und WERTE direkt):
+// ===== allerdings BAD PRACTICE ===== code wird nicht gut lesbar dadurch
+
+let firstName="John", lastName="Doe", age=35;
+console.log(firstName + " " + lastName + " is " + age + " years old.");
+
+// was öfter gemacht wird: in einer Zeile noch undefinierte Variablen deklarieren:
+// ===== let firstName, lastName, age =====
